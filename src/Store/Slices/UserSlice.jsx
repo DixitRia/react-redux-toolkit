cf3ef797b4cf0fcb2f7d3beb52a4cc23afd3cc9c
RiaDixit <106818909+DixitRia@users.noreply.github.com>
2024-06-23 14:41:27 +0530

    Create Store

--- src/Store/Slices/UserSlice.jsx
+++ src/Store/Slices/UserSlice.jsx
@@ -1,13 +1,16 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const users = createSlice({
-    name:"user",
+const userSlice = createSlice({
+    name:"users",
     initialState:[],
     reducers:{
         // main reducer ha iske andar humare micro reducers ayege
         addUsers(state,action){},
         removeUsers(state,action){},
         deleteUsers(state,action){},
-    }
-})
-export {users};
+    },
+});
+
+console.log(userSlice.actions);
+
+export default userSlice.reducer;
